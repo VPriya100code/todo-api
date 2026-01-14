b4a62fc6325ffa18808268010524f9bc5346552b
VPriya100code <priyadharshiniv.cse2024@citchennai.net>
2026-01-14 21:06:19 +0530

    Refactor Register component for improved registration flow
    Updated registration logic to navigate to login after successful registration.

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,54 +1,56 @@
 import { useState } from "react";
-import API from "../services/api";
 import { useNavigate, Link } from "react-router-dom";
-import "../styles.css";
+import API from "../services/api";
 
 function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-const handleRegister = async (e) => {
-  e.preventDefault();
-  try {
-    await API.post("/api/auth/register", { email, password });
-    alert("Registered successfully");
-    navigate("/");
-  } catch (err) {
-    alert(err.response?.data?.message || "Register failed");
-  }
-};
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await API.post("/api/auth/register", {
+        email,
+        password,
+      });
 
- return (
-  <div className="auth-container">
-    <form className="auth-card" onSubmit={handleRegister}>
-      <h2>Login</h2>
+      alert("Registered successfully. Please login.");
+      navigate("/login");
+    } catch (err) {
+      alert(err.response?.data?.message || "Registration failed");
+    }
+  };
 
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        required
-      />
+  return (
+    <div className="auth-container">
+      <form className="auth-card" onSubmit={handleRegister}>
+        <h2>Register</h2>
 
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        required
-      />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
 
-      <button type="submit">Login</button>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
 
-      <p>
-        New user? <Link to="/register">Register</Link>
-      </p>
-    </form>
-  </div>
-);
+        <button type="submit">Register</button>
 
+        <p>
+          Already registered? <Link to="/login">Login</Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
